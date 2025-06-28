e37bc34b6503e8ab36479fc07e4e4b9967b2b7b5
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-06-28 19:37:01 +0100

    Add LinkedList class with push_head method to insert nodes at the beginn

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -3,3 +3,20 @@ class Node:
         self.value = value
         self.previous = None
         self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def push_head(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+
+        if self.head:
+            self.head.previous = new_node
+        else:
+            self.tail = new_node  # if List was empty
+
+        self.head = new_node
+        return new_node  
